0da538f3adf3b4d45a17b535ce1350c88398d5ff
Leonardo Souza <84346349+loesnardo@users.noreply.github.com>
2025-03-20 16:34:15 -0300

    implementar prompt

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { assets } from "@/assets/assets";
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
 import { useState } from "react";
+import PromptBox from "@/components/PromptBox";
 
 export default function Home() {
 
@@ -14,7 +15,7 @@ export default function Home() {
     <div>
       <div className="flex h-screen">
         <Sidebar expand={expand} setExpand={setExpand} />
-        {/* -- side bar-- */}
+
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image
@@ -25,19 +26,19 @@ export default function Home() {
 
           {messages.length === 0 ? (
             <>
-            <div className="flex items-center gap-3">
-              <Image src={assets.logo_icon} alt="" className="h-16"/>
-              <p className="text-2xl font-medium">Hi, I'm  DeepSeek</p>
-            </div>
-            <p className="text-sm mt-2">How can I help you today?</p>
+              <div className="flex items-center gap-3">
+                <Image src={assets.logo_icon} alt="" className="h-16" />
+                <p className="text-2xl font-medium">Hi, I'm  DeepSeek</p>
+              </div>
+              <p className="text-sm mt-2">How can I help you today?</p>
             </>
-          ):
+          ) :
             (
-            <div> </div>
-          )
-        }
-        {/* prompt box */}
-        <p className="text-xs absolute bottom-1 text-gray-500">AI-generated, for reference only</p>
+              <div> </div>
+            )
+          }
+          <PromptBox isLoading={isLoading} setIsLoading={setIsLoading} />
+          <p className="text-xs absolute bottom-1 text-gray-500">AI-generated, for reference only</p>
         </div>
       </div>
     </div>
