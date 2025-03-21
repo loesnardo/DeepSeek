0231e83fd0fb5f3995146ed04579acb757ef5745
Leonardo Souza <84346349+loesnardo@users.noreply.github.com>
2025-03-21 18:16:17 -0300

    Resolver Conexão1

--- config/db.js
+++ config/db.js
@@ -5,8 +5,7 @@ let cached = global.mongoose || { conn: null, promise: null };
 export default async function connectDB() {
     if (cached.conn) return cached.conn;
     if (!cached.promise) {
-        cached.promise = (await mongoose.connect(process.env.MONGODB_URI)).
-            then((mongoose)=>mongoose);
+        cached.promise = (await mongoose.connect(process.env.MONGO_URI)).then((mongoose)=>mongoose);
     }
     try {
         cached.conn = await cached.promise;
